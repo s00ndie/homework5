867d226e1558f954316fab7364ff50f9111abb24
s00ndie <s00ndie@icloud.com>
2024-09-02 17:15:50 +0200

    Add files via upload

--- 5.2.py
+++ 5.2.py
@@ -1,16 +1,9 @@
-answer = 'y'
-while answer == 'y' or answer== 'yes':
-    int1 = int(input('Enter first number:'))
-    int2 = int(input('Enter second number:'))
-    oper = input('Choose operation \'+,-,/,*\':')
-    if oper == '+':
-        print(int1 + int2)
-    elif oper == '-':
-        print(int1 - int2)
-    elif oper == '/' and int2 != 0:
-        print(int1 / int2)
-    elif oper == '*':
-        print(int1*int2)
-    else:
-        print("It's impossible")
-    answer = input('Enter \'yes\' if you want continue:')
+import string
+text = input('Enter your text:')
+punc = str.maketrans('', '', string.punctuation )
+clean_text = text.translate(punc)
+clean_text= clean_text.title().replace(' ', '')
+if len(clean_text) > 140:
+    clean_text=clean_text[:140]
+print(clean_text)
+print(len(clean_text))
