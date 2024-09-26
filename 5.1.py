b3a1f5bccc1a3e15638c3e671207f761737ecaba
s00ndie <s00ndie@icloud.com>
2024-09-26 16:59:24 +0200

    Update and rename 5.1 to 5.1.py

--- 5.1.py
+++ 5.1.py
@@ -2,11 +2,12 @@ import string
 import keyword
 symbols = string.punctuation
 symbols=symbols.replace("_", ' ')
-
 name = input('Enter variable name: ')
 if len(name)>0:
-    if name[0] == '_' or name[0].isalpha() and len(name)==1:
+
+    if name[0] == '_' and len(name) == 1 or name[0].isalpha() and name[0].islower() and len(name)==1:
         print('True')
+
     elif name in keyword.kwlist:
         print(False)
     elif name[0].isalpha() and name.islower():
@@ -24,3 +25,20 @@ if len(name)>0:
         print(False)
 else:
     print(False)
+
+
+# _ => True
+# __ => False
+# ___ => False
+# x => True
+# get_value => True
+# get value => False
+# get!value => False
+# some_super_puper_value => True
+# Get_value => False
+# get_Value => False
+# getValue => False
+# 3m => False
+# m3 => True
+# assert => False
+# assert_exception => True
